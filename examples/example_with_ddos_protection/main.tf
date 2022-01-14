907a297909fc6fbaf0159e0910a9dbd063614d5f
Fabien Zucchet <fabienz@padok.fr>
2022-01-14 16:51:28 +0100

    docs(examples): use resource rg instead of module

--- examples/example_with_ddos_protection/main.tf
+++ examples/example_with_ddos_protection/main.tf
@@ -18,24 +18,23 @@ locals {
   address_space = ["10.0.0.0/8"]
 }
 
-module "resource_group" {
-  source   = "git@github.com:padok-team/terraform-azurerm-resource-group.git?ref=v1.0.0"
+resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_network_ddos_protection_plan" "this" {
   name                = "example-protection-plan"
-  location            = module.resource_group.this.location
-  resource_group_name = module.resource_group.this.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 module "example-virtual-network" {
   source = "../../"
 
   name                                    = "example-virutal-network"
-  resource_group_name                     = module.resource_group.this.name
-  location                                = module.resource_group.this.location
+  resource_group_name                     = azurerm_resource_group.example.name
+  location                                = azurerm_resource_group.example.location
   address_space                           = local.address_space
   azurerm_network_ddos_protection_plan_id = azurerm_network_ddos_protection_plan.this.id
 }
